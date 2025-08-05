0e2b00eb5f626e4fe1dbfa30fa173f4a281b7bcd
Tony <l.matekenya@smartplanblueprint.net>
2025-08-05 15:39:43 +0200

    smart

--- config/bundles.php
+++ config/bundles.php
@@ -15,7 +15,6 @@
 //    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 //];
 
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
