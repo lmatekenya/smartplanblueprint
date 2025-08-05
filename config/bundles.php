f139183ea1667bf72edc0b5fcbdc19750d34564f
Tony <l.matekenya@smartplanblueprint.net>
2025-08-05 14:32:16 +0200

    smart

--- config/bundles.php
+++ config/bundles.php
@@ -14,3 +14,15 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
+
+
+//return [
+//    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+//    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+//    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+//    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+//    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+//    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+////    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true], // Remove this line for production
+//    // Ensure no DebugBundle or WebProfilerBundle entries exist
+//];
