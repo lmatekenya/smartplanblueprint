8e76f1ca2564ed1e250b504c4e2f2a2d4f739aff
Tony <l.matekenya@smartplanblueprint.net>
2025-08-06 09:04:36 +0200

    smart

--- config/bundles.php
+++ config/bundles.php
@@ -15,14 +15,23 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
 
-
 //return [
+//    // Core bundles
 //    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-//    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-//    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 //    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 //    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+//    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+//    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 //    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-////    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true], // Remove this line for production
-//    // Ensure no DebugBundle or WebProfilerBundle entries exist
+//
+//    // Additional features
+//    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+//    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+//    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+//
+//    // Development and testing bundles
+//    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+//    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+//    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => true],
 //];
+
